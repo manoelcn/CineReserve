a6ebcdc827dd9308e9e9cf53d5fca4b05092de87
manoel cândido <candidomanoel78@gmail.com>
2026-03-16 17:20:54 -0300

    chore: improve seat and reservation admin configuration

--- reservations/admin.py
+++ reservations/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from reservations.models import Seat, Reservation
 
 
-admin.site.register(Seat)
-admin.site.register(Reservation)
+@admin.register(Seat)
+class SeatAdmin(admin.ModelAdmin):
+    list_display = ('id', 'session', 'seat_number', 'status')
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'seat', 'reserved_until', 'is_active')
