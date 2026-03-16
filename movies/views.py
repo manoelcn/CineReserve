0c4d67ddead6a0f5826e85902af785ed54b7e195
manoel cândido <candidomanoel78@gmail.com>
2026-03-16 13:43:11 -0300

    feat: add movie and session list views

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import AllowAny
+from movies.serializers import MovieSerializer, SessionSerializer
+from movies.models import Movie, Session
 
-# Create your views here.
+
+class MovieListView(ListAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    permission_classes = (AllowAny,)
+
+
+class SessionListView(ListAPIView):
+    serializer_class = SessionSerializer
+    permission_classes = (AllowAny,)
+
+    def get_queryset(self):
+        movie_id = self.kwargs.get('movie_id')
+        self.queryset = Session.objects.filter(movie_id=movie_id)
+        return self.queryset
