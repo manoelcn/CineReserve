8add886312924f63d5f3dc31d1aa55ae6a781287
manoel cândido <candidomanoel78@gmail.com>
2026-03-16 14:37:38 -0300

    feat: add seat and reservation models

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from authentication.models import User
+from movies.models import Session
 
-# Create your models here.
+STATUS_CHOICES = [
+    ('available', 'Available'),
+    ('reserved', 'Reserved'),
+    ('purchased', 'Purchased'),
+]
+
+class Seat(models.Model):
+    session = models.ForeignKey(Session, on_delete=models.CASCADE, related_name='seats')
+    seat_number = models.CharField(max_length=20)
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='available')
+
+
+class Reservation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reservations')
+    seat = models.ForeignKey(Seat, on_delete=models.CASCADE, related_name='reservations')
+    reserved_until = models.DateTimeField()
+    is_active = models.BooleanField(default=True)
