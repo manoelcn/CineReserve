18e7f07762431af1dedc7894690b9dbfeaab6b6f
manoel cândido <candidomanoel78@gmail.com>
2026-03-16 13:24:36 -0300

    feat: add movie and session models

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    genre = models.CharField(max_length=100)
+    duration = models.IntegerField()
+    banner = models.URLField()
+
+
+class Session(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='session_movie')
+    room = models.CharField(max_length=100)
+    session_datetime = models.DateTimeField()
+    total_seats = models.IntegerField()
