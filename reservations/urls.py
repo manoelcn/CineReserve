ee315c97c31af47c053da171da527d72d512551c
manoel cândido <candidomanoel78@gmail.com>
2026-03-16 17:30:02 -0300

    feat: add tickets url

--- reservations/urls.py
+++ reservations/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from reservations.views import SeatListView, ReservationCreateView, CheckoutView
+from reservations.views import SeatListView, ReservationCreateView, CheckoutView, TicketListView
 
 urlpatterns = [
     path('sessions/<int:session_id>/seats/', SeatListView.as_view(), name='seat-list'),
     path('reservations/', ReservationCreateView.as_view(), name='reservation-create'),
     path('reservations/<int:reservation_id>/checkout/', CheckoutView.as_view(), name='checkout'),
+    path('tickets/', TicketListView.as_view(), name='ticket-list'),
 ]
