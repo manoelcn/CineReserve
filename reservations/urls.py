0055253ba20787eaaca398d87512c0ec08e19b02
manoel cândido <candidomanoel78@gmail.com>
2026-03-16 17:20:10 -0300

    feat: add checkout url

--- reservations/urls.py
+++ reservations/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from reservations.views import SeatListView, ReservationCreateView
+from reservations.views import SeatListView, ReservationCreateView, CheckoutView
 
 urlpatterns = [
     path('sessions/<int:session_id>/seats/', SeatListView.as_view(), name='seat-list'),
     path('reservations/', ReservationCreateView.as_view(), name='reservation-create'),
+    path('reservations/<int:reservation_id>/checkout/', CheckoutView.as_view(), name='checkout'),
 ]
