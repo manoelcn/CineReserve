3449cffd436a55b2a144eb89269fdb5ca6e74268
manoel cândido <candidomanoel78@gmail.com>
2026-03-16 15:37:49 -0300

    chore: add __str__ method to movie and session models

--- movies/models.py
+++ movies/models.py
@@ -8,9 +8,15 @@ class Movie(models.Model):
     duration = models.IntegerField()
     banner = models.URLField()
 
+    def __str__(self):
+        return self.title
+
 
 class Session(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='sessions')
     room = models.CharField(max_length=100)
     session_datetime = models.DateTimeField()
     total_seats = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.movie.title} -  {self.session_datetime}"
