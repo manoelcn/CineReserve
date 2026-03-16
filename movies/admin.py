8c470a942251d636ce539161975da3d8843a15b0
manoel cândido <candidomanoel78@gmail.com>
2026-03-16 17:20:36 -0300

    chore: improve movie and session admin configuration

--- movies/admin.py
+++ movies/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 from movies.models import Movie, Session
 
 
-admin.site.register(Movie)
-admin.site.register(Session)
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'genre', 'duration')
+    search_fields = ('title',)
+
+
+@admin.register(Session)
+class SessionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'movie', 'room', 'session_datetime', 'total_seats')
