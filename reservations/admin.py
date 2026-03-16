9dbf6e1a5665abee3b713c16b23eb93abe1af1a6
manoel cândido <candidomanoel78@gmail.com>
2026-03-16 17:51:06 -0300

    chore: add ticket to admin configuration

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from reservations.models import Seat, Reservation
+from reservations.models import Seat, Reservation, Ticket
 
 
 @admin.register(Seat)
@@ -10,3 +10,8 @@ class SeatAdmin(admin.ModelAdmin):
 @admin.register(Reservation)
 class ReservationAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'seat', 'reserved_until', 'is_active')
+
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('id', 'reservation', 'user', 'code', 'created_at')
