d52ae70b1112fc9383d9418e2aa20420e108b2f0
manoel cândido <candidomanoel78@gmail.com>
2026-03-16 14:42:04 -0300

    feat: add seat and reservation serializers

---
+++ reservations/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from reservations.models import Seat, Reservation
+
+
+class SeatSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Seat
+        fields = '__all__'
+
+
+class ReservationSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Reservation
+        fields = '__all__'
+        extra_kwargs = {
+            'reserved_until': {'read_only': True}
+        }
