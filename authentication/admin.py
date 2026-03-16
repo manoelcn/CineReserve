5bbcb47f0324f2504b29821614342b3455121ce4
manoel cândido <candidomanoel78@gmail.com>
2026-03-16 17:50:48 -0300

    chore: add user admin configuration

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from authentication.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'username', 'is_active', 'is_staff')
+    search_fields = ('username',)
