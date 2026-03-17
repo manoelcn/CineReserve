6076f3455395c3a9bb0f53f5117e2f37c87f95f9
manoel cândido <candidomanoel78@gmail.com>
2026-03-17 13:12:38 -0300

    feat: update reservations urls to use reservationlistcreateview

--- reservations/urls.py
+++ reservations/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from reservations.views import SeatListView, ReservationCreateView, CheckoutView, TicketListView
+from reservations.views import SeatListView, ReservationListCreateView, CheckoutView, TicketListView
 
 urlpatterns = [
     path('sessions/<int:session_id>/seats/', SeatListView.as_view(), name='seat-list'),
-    path('reservations/', ReservationCreateView.as_view(), name='reservation-create'),
+    path('reservations/', ReservationListCreateView.as_view(), name='reservation-create-list'),
     path('reservations/<int:reservation_id>/checkout/', CheckoutView.as_view(), name='checkout'),
     path('tickets/', TicketListView.as_view(), name='ticket-list'),
 ]
